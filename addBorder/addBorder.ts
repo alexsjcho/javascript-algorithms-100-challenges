28069c7609e3dcf36a50fbaf18a3764c3f33aefd
Alex Cho <alexsjcho@gmail.com>
2019-05-12 16:12:41 -0700

    finished addBorder challenge

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,18 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[]): any {
+  const wall = "*".repeat(picture[0].length + 2);
 
+  picture.unshift(wall);
+  picture.push(wall);
+
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+  }
+
+  return picture;
 }
 
 // console.log(addBorder(["abc", "ded"]));
+
+//The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+//The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+//The repeat() method constructs and returns a new string which contains the specified number of copies of the string on which it was called, concatenated together.
