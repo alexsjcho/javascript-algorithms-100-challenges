795bdef11b790146e33d8bb9791a6b17749a9905
Alex Cho <alexsjcho@gmail.com>
2019-05-14 15:14:44 -0700

    finished allLongestStrings challenge

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let longestLength = 0;
+  const longestWords = [];
 
+  inputArray.forEach((word: string) => {
+    longestLength = longestLength < word.length ? word.length : longestLength;
+  });
+
+  inputArray.forEach((word: string) => {
+    if (word.length === longestLength) {
+      longestWords.push(word);
+    }
+  });
+
+  return longestWords;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+
+//The forEach() method executes a provided function once for each array element.
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
