94d9a82e58a447809dbd980d28eb4def84154075
Alex Cho <alexsjcho@gmail.com>
2019-05-15 17:06:17 -0700

    finished areEquallyStrong challenge

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,17 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight;
 
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
