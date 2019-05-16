c5c4aa22f9a73dd67549d13aa2e0d0ec55c7cfed
Alex Cho <alexsjcho@gmail.com>
2019-05-15 17:01:51 -0700

    finished alternatingSums challenge

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
+  let evenSum = 0;
+  let oddSum = 0;
 
+  a.forEach((element, index) => {
+    //0 represents an even sum
+    if (index % 2 === 0) {
+      evenSum += element;
+    } else {
+      oddSum += element;
+    }
+  });
+  return [evenSum, oddSum];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
