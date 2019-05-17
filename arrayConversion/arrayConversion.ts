1e5b0f42407fedc679a02ca9c330018e47cc24e5
Alex Cho <alexsjcho@gmail.com>
2019-05-16 21:13:27 -0700

    finished arrayConversion challenge

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,27 @@
 function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
 
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+  return inputArray[0];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
+
+function sumProduct(nums: number[], isOdd: boolean): number[] {
+    const sumProducts: number[] = [];
+    if (isOdd) {
+      for (let i = 0; i < nums.length; i += 2) {
+        sumProducts.push(nums[i] + nums[i + 1]);
+      } else {
+          for (let i = 0; i < nums.length; i += 2) {
+              sumProducts.push(nums[i] * nums[i + 1]);
+            } 
+      }
+      return sumProducts;
+  }
+  
