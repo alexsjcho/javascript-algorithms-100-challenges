6a2c7464faa4c8ca593576c6e8bb5ef6fd9e00cf
Alex Cho <alexsjcho@gmail.com>
2019-05-16 21:18:19 -0700

    finished arrayMaxConsecutiveSum challenge

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,19 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+  let sum = 0;
+  let max = 0;
 
+  for (let i = 0; i < k; i++) {
+    sum += inputArray[i];
+  }
+  max = sum;
+
+  for (let i = k; i < inputArray.length; i++) {
+    sum -= inputArray[i - k];
+    sum += inputArray[i];
+    if (sum > max) {
+      max = sum;
+    }
+  }
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
