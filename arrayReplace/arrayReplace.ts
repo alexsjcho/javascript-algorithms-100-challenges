d175c37c81088e37f5b113ea91c2f735a571b8b4
Alex Cho <alexsjcho@gmail.com>
2019-05-20 14:59:55 -0700

    finished arrayReplace challenge

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,15 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  inputArray.forEach((element, index) => {
+    if (element === elemToReplace) {
+      inputArray[index] = substitutionElem;
+    }
+  });
+
+  return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
