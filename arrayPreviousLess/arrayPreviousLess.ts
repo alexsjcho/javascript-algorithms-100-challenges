a3a4c81c7b12c2bf2c2859267e06c2b19901597c
Alex Cho <alexsjcho@gmail.com>
2019-05-20 14:53:58 -0700

    finished arrayPreviousLess challenge

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,19 @@
 function arrayPreviousLess(items: number[]): number[] {
+  const lessThanList: number[] = [];
 
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        lessThanList.unshift(items[j]);
+        break;
+      } else if (j === 0) {
+        lessThanList.unshift(-1);
+      }
+    }
+  }
+  return lessThanList;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+
+//The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
