654fb4f777142d88ade2aadcca3ba9c197d61b76
Alex Cho <alexsjcho@gmail.com>
2019-05-23 16:25:53 -0700

    finished boxBlur challenge

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,28 @@
 function boxBlur(image: number[][]): number[][] {
+  const res = [];
 
+  for (let y = 0; y < image.length - 2; y++) {
+    const line = [];
+
+    for (let x = 0; x < image[y].length - 2; x++) {
+      let sum = 0;
+      let count = 0;
+      for (let a = y; a < y + 3; a++) {
+        for (let b = x; b < x + 3; b++) {
+          sum += image[a][b];
+          count++;
+        }
+      }
+      line.push(Math.floor(sum / count));
+    }
+  }
+
+  res.push(line);
+
+  return res;
 }
 
-console.log(boxBlur([[1, 1, 1], 
-    [1, 7, 1], 
-    [1, 1, 1]]));
+console.log(boxBlur([[1, 1, 1], [1, 7, 1], [1, 1, 1]]));
+
+//The Math.floor() function returns the largest integer less than or equal to a given number.
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
