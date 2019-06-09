5aac569a197cf084ef21fa75fd33808acd55457e
Alex Cho <alexsjcho@gmail.com>
2019-06-09 16:21:20 -0700

    finished candy challenge

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,9 @@
 function candies(n: number, m: number): number {
+  const candy = Math.floor(m / 3);
 
+  return candy * n;
 }
+
+//The Math.floor() function returns the largest integer less than or equal to a given number.
 
 console.log(candies(3, 10));
