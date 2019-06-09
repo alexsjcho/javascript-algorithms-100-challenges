422151e63ed9d19e8627a4bfd66a41c87c2ec26a
Alex Cho <alexsjcho@gmail.com>
2019-06-09 16:25:28 -0700

    finished caseInsensitivePalindrome challenge

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,11 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+  const originalLowerCase = inputString.toLowerCase();
+  const reversedWord = originalLowerCase
+    .split('')
+    .reverse()
+    .join('');
 
+  return originalLowerCase === reversedWord;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
