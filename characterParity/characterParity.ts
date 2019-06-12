4e84f3bc72b3685e83e21fb7adb8dfb77f728dcc
Alex Cho <alexsjcho@gmail.com>
2019-06-11 18:59:20 -0700

    finished characterParity challenge

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,13 @@
 function characterParity(symbol: string): string {
-   
+  const result = parseInt(symbol);
+
+  return isNaN(result) ? 'not a digit' : result % 2 === 0 ? 'even' : 'odd';
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+//The isNaN() function determines whether a value is NaN or not. Note: coercion inside the isNaN function has interesting rules; you may alternatively want to use Number.isNaN(), as defined in ECMAScript 2015.
+
+//The parseInt() function parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
