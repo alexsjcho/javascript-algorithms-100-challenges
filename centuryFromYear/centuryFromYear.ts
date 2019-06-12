03ebb1d6897a219a067ba92b8d779f5df66092bb
Alex Cho <alexsjcho@gmail.com>
2019-06-11 18:53:31 -0700

    finished centuryFromYear challenge

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,5 +1,11 @@
 function centuryFromYear(year: number): number {
- 
+  const century = year / 100;
+
+  if (year % 100 === 0) {
+    return century;
+  }
+
+  return Math.floor(century) + 1;
 }
 
 console.log(centuryFromYear(1905));
