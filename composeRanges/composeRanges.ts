0623afac5983b715aae4fb759cbc9afaf5369d80
Alex Cho <alexsjcho@gmail.com>
2019-06-28 07:36:28 -0700

    finished composeRanges challenge

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,6 +1,24 @@
 function composeRanges(nums: number[]): string[] {
+  const ranges: any[] = [{ start: nums[0], end: nums[0] }];
+  if (nums.length < 1) {
+    return [];
+  }
 
+  for (let i = 1; i < nums.length; i++) {
+    if (ranges[ranges.length - 1].end + 1 === nums[i]) {
+      ranges[ranges.length - 1].end = nums[i];
+    } else {
+      ranges.push({ start: nums[i], end: nums[i] });
+    }
+  }
+  for (let i = 0; i < ranges.length; i++) {
+    if (ranges[i].start !== ranges[i].end) {
+      ranges[i] = `${ranges[i].start}-> ${ranges[i].end}`;
+    } else {
+      ranges[i] = ranges[i].start.toString();
+    }
+  }
+  return ranges;
 }
-
 
 console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
