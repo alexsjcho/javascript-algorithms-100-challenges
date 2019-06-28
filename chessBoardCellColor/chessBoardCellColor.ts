4bb49c0ea6f2a3ddc260b82d65586e754f2006c7
Alex Cho <alexsjcho@gmail.com>
2019-06-28 06:51:28 -0700

    finished chessBoardCellColor challenge

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,5 +1,19 @@
 function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8
+  };
 
+  const total1 = (board[cell1[0]] + parseInt(cell1[1])) % 2;
+  const total2 = (board[cell2[0]] + parseInt(cell2[1])) % 2;
+
+  return total1 === total2;
 }
 
 console.log(chessBoardCellColor('A1', 'C3'));
