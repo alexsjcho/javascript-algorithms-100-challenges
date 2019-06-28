b2395cf9e296ee8eebf92ba5756eb63dd4a43329
Alex Cho <alexsjcho@gmail.com>
2019-06-28 06:57:22 -0700

    finished chunkyMonkey challenge

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,13 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
+  const nestedArray = [];
+  let count = 0;
 
+  while (count < arr.length) {
+    nestedArray.push(arr.slice(count, (count += size)));
+  }
+
+  return nestedArray;
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
