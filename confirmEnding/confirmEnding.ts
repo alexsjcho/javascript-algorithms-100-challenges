6620007b40bc77d8d3f6847c7e21ca2f9ca33492
Alex Cho <alexsjcho@gmail.com>
2019-06-29 05:15:24 -0700

    finished confirmEnding challenge

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,8 @@
 function confirmEnding(str: string, target: string) {
-   
+  const start = str.length - target.length;
+
+  return str.substr(start) === target;
 }
 
-console.log(confirmEnding("Abstraction", "action"));
-console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'));
+console.log(confirmEnding('Open sesame', 'pen'));
