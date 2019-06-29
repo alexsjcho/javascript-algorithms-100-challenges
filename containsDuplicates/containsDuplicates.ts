d684d629e22657c50ec531a012447c53bbce3202
Alex Cho <alexsjcho@gmail.com>
2019-06-29 05:49:34 -0700

    finished containsDuplicates challenge

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,13 @@
 function containsDuplicates(a: number[]): boolean {
+  a = a.sort((a, b) => a - b);
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === a[i + 1]) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
