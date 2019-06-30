1cf03a949f0dd1238dbad7740063a9c334b2376a
Alex Cho <alexsjcho@gmail.com>
2019-06-30 10:30:47 -0700

    finished crossingSum challenge

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,12 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+  const columnTotal = matrix[a].reduce((a, b) => a + b);
+  let rowTotal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    rowTotal += i !== a ? matrix[i][b] : 0;
+  }
+
+  return columnTotal + rowTotal;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 1, 3));
