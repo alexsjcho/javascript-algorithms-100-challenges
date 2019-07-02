fed9bcaac9ed31a270ff81bc5f59a8151cf8633a
Alex Cho <alexsjcho@gmail.com>
2019-07-01 17:47:53 -0700

    finished depositProfit challenge

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,17 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let year = 0;
+  let account = deposit;
+
+  while (threshold > account) {
+    account += account * (rate / 100);
+    year++;
+  }
+
+  return year;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
