bb29b866e449d357d16001bc7169f6ea85267a71
Alex Cho <alexsjcho@gmail.com>
2019-07-01 18:09:24 -0700

    finished differentSymbolNaive challenge

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,29 @@
 function differentSymbolsNaive(s: string): number {
- 
+  const uniqueChars = new Set();
+  const inputChars = s.split('');
+
+  inputChars.forEach((char) => {
+    uniqueChars.add(char);
+  });
+
+  console.log(uniqueChars);
+  return uniqueChars.size;
 }
 
 console.log(differentSymbolsNaive('cabca'));
+
+//1st solution
+// function differentSymbolsNaive(s: string): number {
+//   const uniqueChars: string[] = [];
+//   const inputChars: string[] = s.split('');
+
+//   inputChars.forEach((char) => {
+//     if (!uniqueChars.includes(char)) {
+//       uniqueChars.push(char);
+//     }
+//   });
+
+//   return uniqueChars.length;
+// }
+
+// console.log(differentSymbolsNaive('cabca'));
