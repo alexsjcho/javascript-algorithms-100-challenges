a7b485e16558b26366fcd31dedcb6644a0e36aa2
Alex Cho <alexsjcho@gmail.com>
2019-07-03 07:31:09 -0700

    finished domtainType challenge

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,24 @@
 function domainType(domains: string[]): string[] {
+  const domainType: string[] = [];
 
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i].split('.');
+    const lastDomain = domain[domain.length - 1];
+
+    if (lastDomain === 'org') {
+      domainType.push('organization');
+    } else if (lastDomain === 'com') {
+      domainType.push('commericial');
+    } else if (lastDomain === 'net') {
+      domainType.push('network');
+    } else if (lastDomain === 'info') {
+      domainType.push('information');
+    }
+  }
+
+  return domainType;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+);
