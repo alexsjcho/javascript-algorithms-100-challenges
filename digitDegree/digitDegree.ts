9a2c0c78a7af96f5d203656d6ad2dca0ee31c2cd
Alex Cho <alexsjcho@gmail.com>
2019-07-03 07:22:43 -0700

    finished digitDegree challenge

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,33 @@
 function digitDegree(n: number): number {
-  
+  let count = 0;
+  let currentNumber = n;
+
+  if (n <= 9) {
+    return count;
+  } else {
+    while (true) {
+      count++;
+      currentNumber = getDigit(currentNumber);
+
+      if (currentNumber <= 9) {
+        break;
+      }
+    }
+  }
+
+  return count;
+}
+
+function getDigit(num: number): number {
+  const nums: number[] = num
+    .toString()
+    .split('')
+    .map((element) => {
+      parseInt(element);
+    });
+  return nums.reduce((a: any, b: any) => {
+    return a + b;
+  });
 }
 
 console.log(digitDegree(5));
