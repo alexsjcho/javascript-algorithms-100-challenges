993885c6f69f68efa089a91c9aad2d43fc35bcff
Alex Cho <alexsjcho@gmail.com>
2019-07-04 19:59:51 -0700

    finished factorializedNumber challenge

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,11 @@
 function factorializeANumber(num: number): number {
+  let total = 1;
 
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 }
 
 console.log(factorializeANumber(5));
