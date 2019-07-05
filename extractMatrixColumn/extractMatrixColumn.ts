f320bec552aee7280c3c4fb7baffb2dfba8c2d22
Alex Cho <alexsjcho@gmail.com>
2019-07-04 19:54:37 -0700

    finished extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,9 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+  const matrixColumn: number[] = [];
 
+  matrix.forEach((row) => matrixColumn.push(row[column]));
+
+  return matrixColumn;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
