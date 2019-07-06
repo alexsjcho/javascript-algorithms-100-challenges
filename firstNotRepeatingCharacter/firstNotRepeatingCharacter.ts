f5e7d80a7278126d64389ed324fad32e6c57f4e0
Alex Cho <alexsjcho@gmail.com>
2019-07-06 16:03:07 -0700

    finished firstNotRepeatingCharacter challenge

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,5 +1,29 @@
 function firstNotRepeatingCharacter(s: string): string {
+  const chars: string[] = s.split('');
+  let duplicates = {};
+  let answer = '_';
+  let indexAnswer = Number.MAX_SAFE_INTEGER;
 
+  chars.forEach((element, index) => {
+    if (!duplicates.hasOwnProperty(element)) {
+      duplicates[element] = {
+        count: 1,
+        index
+      };
+    } else {
+      duplicates[element].count++;
+      duplicates[element].index = index;
+    }
+  });
+
+  for (const key in duplicates) {
+    if (duplicates[key].count === 1 && duplicates[key].index < indexAnswer) {
+      answer = key;
+      indexAnswer = duplicates[key].index;
+    }
+  }
+
+  return answer;
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
