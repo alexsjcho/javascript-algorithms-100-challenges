1d0dcd9ce7dec087de67824168a67dba64caf936
Alex Cho <alexsjcho@gmail.com>
2019-07-05 18:06:29 -0700

    finished findClosestPair challenge

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,5 +1,19 @@
 function findClosestPair(numbers: number[], sum: number): number {
-   
+  let distance = -1;
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length - 1; j++) {
+      const distanceSum = numbers[i] + numbers[j];
+      const absDistance = Math.abs(i - j);
+      if (sum === distanceSum) {
+        if (distance === -1 || absDistance < distance) {
+          distance = absDistance;
+        }
+      }
+    }
+  }
+
+  return distance;
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
