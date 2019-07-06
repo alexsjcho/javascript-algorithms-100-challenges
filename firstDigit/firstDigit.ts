63023b4d161cd746082a17ece779df5c3f4c90a2
Alex Cho <alexsjcho@gmail.com>
2019-07-06 15:35:36 -0700

    finished firstDigit challenge

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,13 @@
 function firstDigit(inputString: string): string {
-
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const chars = inputString.split('');
+  
+  for(let i = 0; i < chars.length; i++)
+  {
+    if(digits.includes(chars[i]) {
+      return chars[i];
+    })
+  }
 }
 
 console.log(firstDigit('var_1__Int'));
