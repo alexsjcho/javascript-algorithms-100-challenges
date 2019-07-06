c6a43c728e088e02d91de9b1e3235a8df861cb36
Alex Cho <alexsjcho@gmail.com>
2019-07-05 18:00:50 -0700

    finished fermactor challenge

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,12 @@
 function fermactor(n: number): number[] {
-
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < i; j++) {
+      const total = Math.pow(i, 2) - Math.pow(j, 2);
+      if (total === n) {
+        return [i, j];
+      }
+    }
+  }
 }
 
 console.log(fermactor(15));
