2821757a626243028c2a0e0cbb65ce37e5e27538
Alex Cho <alexsjcho@gmail.com>
2019-07-08 20:28:23 -0700

    finished growingPlant challenge

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,20 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  let days = 0;
+  let totalHeight = 0;
 
+  while (totalHeight < desiredHeight) {
+    days++;
+    totalHeight += upSpeed;
+    if (totalHeight < desiredHeight) {
+      totalHeight -= downSpeed;
+    }
+  }
+
+  return days;
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
