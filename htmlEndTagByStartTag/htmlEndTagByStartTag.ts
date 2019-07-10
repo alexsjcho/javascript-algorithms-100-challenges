b8d4eb857044d976ec4048c34307c4f368d284ce
Alex Cho <alexsjcho@gmail.com>
2019-07-10 15:24:03 -0700

    finished hmtlEndTagByStartTag challenge

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,16 @@
 function htmlEndTagByStartTag(startTag: string): string {
+  const splitString = startTag.split(' ');
+  const splitTag = splitString[0].toString().split('');
+  let endTag = '</';
 
+  for (let i = 1; i < splitTag.length; i++) {
+    endTag += splitTag[i];
+  }
+
+  endTag = endTag[endTag.length - 1] === '>' ? endTag : (endTag += '>');
+
+  return endTag;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag('<i>'));
