6efc4e0fc9698bd1e3e8f54cbe53e65492a8af8b
Alex Cho <alexsjcho@gmail.com>
2019-07-09 18:27:22 -0700

    finished houseOfCats challenge

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,5 +1,16 @@
 function houseOfCats(legs: number): number[] {
+  const peopleCount: number[] = [];
 
+  if (legs === 2) {
+    return [1];
+  }
+
+  while (legs >= 0) {
+    peopleCount.unshift(legs / 2);
+    legs -= 4;
+  }
+
+  return peopleCount;
 }
 
 console.log(houseOfCats(6));
