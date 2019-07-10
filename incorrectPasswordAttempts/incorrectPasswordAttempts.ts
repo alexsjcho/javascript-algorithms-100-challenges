006b7e2f592e3c2de1b2d642a95cac584bb23d88
Alex Cho <alexsjcho@gmail.com>
2019-07-10 15:29:27 -0700

    finished incorrectPasswordAttempts challenge

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,32 @@
-function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+function incorrectPasscodeAttempts(
+  passcode: string,
+  attempts: string[]
+): boolean {
+  let faildAttempts = 0;
 
+  for (let attempt of attempts) {
+    faildAttempts = attempt === passcode ? 0 : faildAttempts++;
+
+    if (faildAttempts === 10) {
+      return true;
+    }
+  }
+  return false;
 }
 
-console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts('1111', [
+    '1111',
+    '4444',
+    '9999',
+    '3333',
+    '8888',
+    '2222',
+    '7777',
+    '0000',
+    '6666',
+    '7285',
+    '5555',
+    '1111'
+  ])
+);
