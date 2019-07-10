0105a886efbdfae2743bf596ad2828c5f716c0d7
Alex Cho <alexsjcho@gmail.com>
2019-07-09 18:18:59 -0700

    finished houseNumberSum challenge

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,15 @@
 function houseNumbersSum(inputArray: number[]): number {
+  let total = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === 0) {
+      return total;
+    }
+
+    total += inputArray[i];
+  }
+
+  return total;
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
