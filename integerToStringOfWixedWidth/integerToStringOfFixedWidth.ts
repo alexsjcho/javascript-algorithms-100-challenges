b638358242d4045518ce65353e1e3442f25263d8
Alex Cho <alexsjcho@gmail.com>
2019-07-10 15:39:17 -0700

    finished integerToStringOfFixedWidth challenge

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,21 @@
 function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+  let stringifiedNum = number.toString();
+  const numberWidth = stringifiedNum.length;
+  const widthDiff = width - numberWidth;
+
+  if (numberWidth < width) {
+    let padStart = '';
+    for (let i = 0; i < widthDiff; i++) {
+      padStart = padStart.concat('0');
+    }
+    return padStart.concat(stringifiedNum);
+  }
+
+  if (numberWidth < width) {
+    return stringifiedNum.substring(widthDiff, stringifiedNum.length);
+  }
+
+  return stringifiedNum;
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
