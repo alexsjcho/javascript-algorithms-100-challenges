4bb42e2060acf8744445d23c44e26e2a38fea7e2
Alex Cho <alexsjcho@gmail.com>
2019-07-11 14:00:04 -0700

    finished incrementalBackup challenge

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,11 +1,28 @@
-function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
+function incrementalBackups(
+  lastBackupTime: number,
+  changes: number[][]
+): number[] {
+  const fileIds: number[] = [];
 
+  for (let change of changes) {
+    const fileBackuptime = change[0];
+    const filedId = change[1];
+    if (fileBackuptime > lastBackupTime) {
+      if (!fileIds.includes(filedId)) fileIds.push(filedId);
+    }
+  }
+
+  return fileIds.sort((a, b) => a - b);
 }
 
-console.log(incrementalBackups(461620205, [[461620203, 1], 
-    [461620204, 2], 
+console.log(
+  incrementalBackups(461620205, [
+    [461620203, 1],
+    [461620204, 2],
     [461620205, 6],
-    [461620206, 5], 
-    [461620207, 3], 
-    [461620207, 5], 
-    [461620208, 1]]));
+    [461620206, 5],
+    [461620207, 3],
+    [461620207, 5],
+    [461620208, 1]
+  ])
+);
