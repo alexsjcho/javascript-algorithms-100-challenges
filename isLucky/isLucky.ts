012287d2e42852ce12d392456680c40fb44801d0
Alex Cho <alexsjcho@gmail.com>
2019-07-12 15:34:15 -0700

    finished isLucky challenge

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,18 @@
 function isLucky(n: number): boolean {
-    
+  const luckyN = n.toString();
+  const half = luckyN.length / 2;
+
+  const firstHalf = getTotal(luckyN.substring(0, half));
+  const secondHalf = getTotal(luckyN.substring(half, luckyN.length));
+
+  return firstHalf === secondHalf;
+}
+
+function getTotal(n: string) {
+  return n
+    .split('')
+    .map((char) => parseInt(char))
+    .reduce((num1, num2) => num1 + num2);
 }
 
 console.log(isLucky(1230));
