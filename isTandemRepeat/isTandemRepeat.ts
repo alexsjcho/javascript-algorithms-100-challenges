741f2113081bc3ab7dc5a1ae4688f70de4b945b4
Alex Cho <alexsjcho@gmail.com>
2019-07-16 13:49:55 -0700

    finished isTandemRepeat challenge

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,16 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+  const wordLength = inputString.length;
+
+  if (wordLength % 2 === 0) {
+    const firstHalf = inputString.slice(0, wordLength / 2);
+    const secondHalf = inputString.slice(wordLength / 2, wordLength);
+
+    return firstHalf === secondHalf;
+  }
+  //odd values return false
+  return false;
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('2w2ww'));
