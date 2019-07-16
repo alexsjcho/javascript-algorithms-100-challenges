d5b90210cdc77d073478f90383bf4e54301f3e68
Alex Cho <alexsjcho@gmail.com>
2019-07-16 13:59:06 -0700

    finished largestNumber challenge

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,11 @@
 function largestNumber(n: number): number {
- 
+  let word = '9'.repeat(n);
+
+  // for (let i = 0; i < n; i++) {
+  //   word = word.concat('9');
+  // }
+
+  return parseInt(word);
 }
 
 console.log(largestNumber(2));
